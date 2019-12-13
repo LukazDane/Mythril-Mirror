22c22e8ef3041e483755750f177855e4bff2cceb
Luke <deandrevidal@aol.com>
2019-12-12 21:45:05 -0800

    sheets page created

--- sheets/views.py
+++ sheets/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Character
+from django.template import loader
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the sheets index.")
+    recent_char_list = Character.objects.order_by('-created')
+    # template = loader.get_template('sheets/index.html')
+    context = {'recent_char_list': recent_char_list, }
+    # return HttpResponse(template.render(context, request))
+    return render(request, 'sheets/index.html', context)
+
+
+def detail(request, character_id):
+    return HttpResponse("You're looking at character: %s." % character_id)
